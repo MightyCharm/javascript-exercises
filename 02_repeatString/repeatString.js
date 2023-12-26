77ad1c18da84cdcea28469c8d6e89365a0112225
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-26 21:36:13 +0100

    Edit repeatString.js and repeatString.spec.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let resultString = "";
+    let i = 0;
+    
+    // checks if num is negative
+    if(num < 0) {
+        return "ERROR";
+    }
+    
+    while(i < num) {
+        resultString += string;
+        i++;
+    }
+    return resultString;   
 };
 
 // Do not edit below this line
