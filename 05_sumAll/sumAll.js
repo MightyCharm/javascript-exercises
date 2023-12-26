0d3bdba9817172a36b1ca5c9b8a7e8db791f6fd1
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-27 00:49:26 +0100

    Edit sumAll.js and sumAll.spec.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(...num) {
+    let start;
+    let stop;
 
+    // check if value are numbers
+    if(Number.isInteger(num[0]) === false || Number.isInteger(num[1]) === false ) {
+        return "ERROR";
+    }
+    // check for negative numbers
+    if(num[0] < 0 || num[1] < 0) {
+        return "ERROR";
+    }
+    // check if first number is smaller than second
+    if(num[0] < num[1]) {
+        start = num[0];
+        stop = num[1];
+    }
+    else {
+        start = num[1];
+        stop = num[0];
+    }
+    let sum = 0;
+    for(let i=start; i <= stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
