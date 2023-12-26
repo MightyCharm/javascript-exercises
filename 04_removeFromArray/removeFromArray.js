343fb13e45efd0eeeabcd79192675f9cbe818167
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-26 23:45:38 +0100

    Edit removeFromArray.js, removeFromArray.spec.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...items) {
+    
+    items.forEach( (item) => {
+        let indexRemove = arr.indexOf(item);
+        if(indexRemove > -1) {
+            arr.splice(indexRemove, 1);
+        };
+    });
+    return arr;  
 };
 
 // Do not edit below this line
