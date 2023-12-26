47d0132e9b94edd01c443bfca6b1d88bd7dfb0b2
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-26 21:58:24 +0100

    Edit reverseString.js and reverseString.spec.js

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let splitString = string.split(""); // string to array
 
+    let reversedString = "";
+    for(let i=splitString.length -1; i >= 0; i--) {
+        reversedString += splitString[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
