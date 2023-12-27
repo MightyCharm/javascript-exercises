56ac513099f4d814e82b7f2a88ee4ed4e16b1a7f
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-27 03:40:35 +0100

    Edit calculator.js and calculator.spec.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,59 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2; 
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0;
+  arr.forEach( item => {
+    sum += item;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = 0;
+  for(let i=0; i < arr.length; i++) {
+    if(i === 0) {
+        result = arr[i];
+        continue;
+    };
+    result *= arr[i];
+  };
+  return result;
 };
 
-const power = function() {
-	
+const power = function(...items) {
+	let number = items[0];
+  let pow = items[1];
+  let result;
+  for(let i=0; i < pow; i++) {
+    if(i === 0) {
+      result = number;
+      continue;
+    };
+    result *= number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if(number === 0 || number === 1) {
+    return 1;
+  }
+
+  let result = 0;
+  for(let i=0; i < number; i++) {
+    if(i === 0) {
+      result = number;
+      continue;
+    };
+    result *= number - i;
+  };
+  return result;
 };
 
 // Do not edit below this line
