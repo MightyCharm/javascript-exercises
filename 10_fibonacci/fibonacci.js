666d539498e8e247c80a69c85c962540906c1e39
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-01-11 10:02:38 +0100

    Edit fibonacci.js and fibonacci.spec.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    if(index === 0) {
+        return 0;
+    };
+    if(index === 1) {
+        return 1;
+    }
+    if(index < 0) {
+        return "OOPS";
+    };
 
+    let a = 0;
+    let b = 1;
+    let next;
+    for(let i=1; i < index; i++){
+        next = a + b;
+        a = b;
+        b = next;
+    }; 
+    return next;
 };
 
 // Do not edit below this line
