7b28103ce2e6b06c8549e6a47ecc3a75963a65f9
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-06-22 15:09:00 +0200

    feat(recursion): solve 1_factorial and sync legacy files

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,7 @@
-const factorial = function() {
-
+const factorial = function (num) {
+  if (num < 0 || !Number.isInteger(num)) return;
+  if (num <= 0) return 1;
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
