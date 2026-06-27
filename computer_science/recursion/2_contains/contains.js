5586f929abcf2f2dfcefe79caec173208f4b9358
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-06-27 17:22:58 +0200

    feat(recursion): solve 2_contains

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,4 +1,21 @@
-const contains = function () {};
+const contains = function (data, target_value) {
+  if (Number.isNaN(data) && Number.isNaN(target_value)) {
+    return true;
+  }
+
+  if (data === target_value) {
+    return true;
+  }
+
+  if (typeof data === "object" && data !== null) {
+    for (const value of Object.values(data)) {
+      if (contains(value, target_value)) {
+        return true;
+      }
+    }
+  }
+  return false;
+};
 
 // Do not edit below this line
 module.exports = contains;
