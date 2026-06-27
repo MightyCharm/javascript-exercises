6454c36b20194c2661ed0f119189151d69efa41d
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-06-27 23:01:20 +0200

    feat(recursion): solve 3_totalIntegers

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,31 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function (data, count = 0, isInitialCall = true) {
+  if (isInitialCall) {
+    if (typeof data !== "object" || (data === null && !Array.isArray(data))) {
+      return undefined;
+    }
+  }
+
+  if (Number.isInteger(data)) {
+    count += 1;
+    return count;
+  }
+
+  if (Array.isArray(data)) {
+    for (const element of data) {
+      count += totalIntegers(element, 0, false);
+    }
+    return count;
+  }
+
+  if (typeof data === "object" && data !== null) {
+    for (const element of Object.values(data)) {
+      count += totalIntegers(element, 0, false);
+    }
+    return count;
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
