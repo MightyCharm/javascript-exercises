90fafce7cbe2db0bf33b84db009b5db193d87d27
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-06-28 22:08:39 +0200

    feat(recursion): solve 4_permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,24 @@
-const permutations = function() {
-  
+const permutations = function (data) {
+  if (data.length <= 1) {
+    return [data];
+  }
+  const results = [];
+  for (let i = 0; i <= data.length - 1; i++) {
+    const current = data[i];
+    const remaining = [];
+    for (let j = 0; j <= data.length - 1; j++) {
+      const number = data[j];
+      if (number !== current) {
+        remaining.push(number);
+      }
+    }
+    const sub_permutations = permutations(remaining);
+    for (const item of sub_permutations) {
+      results.push([current, ...item]);
+    }
+  }
+  return results;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
