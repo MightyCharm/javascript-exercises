002318595e47a002c09750b4f5dd1214a745bf9c
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2026-07-22 11:50:33 +0200

    feat(recursion): solve 5_pascal

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,17 @@
-const pascal = function() {
-  
+const pascal = function (row) {
+  if (row <= 1) {
+    return [1];
+  }
+  const previousRow = pascal(row - 1);
+  const currentRow = [1];
+
+  for (let i = 0; i < previousRow.length - 1; i++) {
+    currentRow.push(previousRow[i] + previousRow[i + 1]);
+  }
+
+  currentRow.push(1);
+  return currentRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
